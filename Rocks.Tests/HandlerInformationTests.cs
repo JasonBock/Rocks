655fb7b50bb0fa19470b4c51545dd20b4a06551a
Jason Bock <jason@jasonbock.net>
2015-02-19 17:09:17 -0600

    Tests for HandlerInformation
    Pushed tests for HandlerInformationTests

---
+++ Rocks.Tests/HandlerInformationTests.cs
@@ -0,0 +1,75 @@
+﻿using NUnit.Framework;
+using System;
+
+namespace Rocks.Tests
+{
+	[TestFixture]
+	public sealed class HandlerInformationTests
+	{
+		[Test]
+		public void Create()
+		{
+			var information = new HandlerInformation();
+			Assert.IsNull(information.Method, nameof(information.Method));
+			Assert.AreEqual(1, information.ExpectedCallCount, nameof(information.ExpectedCallCount));
+			Assert.AreEqual(0, information.CallCount, nameof(information.CallCount));
+		}
+
+		[Test]
+		public void CreateWithMethod()
+		{
+			var method = new Action(() => { });
+         var information = new HandlerInformation(method);
+			Assert.AreSame(method, information.Method, nameof(information.Method));
+			Assert.AreEqual(1, information.ExpectedCallCount, nameof(information.ExpectedCallCount));
+			Assert.AreEqual(0, information.CallCount, nameof(information.CallCount));
+		}
+
+		[Test]
+		public void CreateWithExpectedCallCount()
+		{
+			var information = new HandlerInformation(2);
+			Assert.IsNull(information.Method, nameof(information.Method));
+			Assert.AreEqual(2, information.ExpectedCallCount, nameof(information.ExpectedCallCount));
+			Assert.AreEqual(0, information.CallCount, nameof(information.CallCount));
+		}
+
+		[Test]
+		public void CreateWithMethodAndExpectedCallCount()
+		{
+			var method = new Action(() => { });
+			var information = new HandlerInformation(method, 2);
+			Assert.AreSame(method, information.Method, nameof(information.Method));
+			Assert.AreEqual(2, information.ExpectedCallCount, nameof(information.ExpectedCallCount));
+			Assert.AreEqual(0, information.CallCount, nameof(information.CallCount));
+		}
+
+		[Test]
+		public void IncrementCallCount()
+		{
+			var information = new HandlerInformation();
+			information.IncrementCallCount();
+
+			Assert.AreEqual(1, information.CallCount, nameof(information.CallCount));
+		}
+
+		[Test]
+		public void Verify()
+		{
+			var information = new HandlerInformation();
+			information.IncrementCallCount();
+			var result = information.Verify();
+
+			Assert.AreEqual(0, result.Count, nameof(result.Count));
+		}
+
+		[Test]
+		public void VerifyWhenExpectedCallCountIsIncorrect()
+		{
+			var information = new HandlerInformation();
+			var result = information.Verify();
+
+         Assert.AreEqual(1, result.Count, nameof(result.Count));
+		}
+	}
+}
--- Rocks/HandlerInformation.cs
+++ Rocks/HandlerInformation.cs
@@ -9,7 +9,6 @@ namespace Rocks
 		private const string ErrorExpectedCallCount = "The expected call count is incorrect. Expected: {0}, received: {1}.";
 
 		private int callCount;
-		private uint expectedCallCount;
 
 		public HandlerInformation()
 			: this(null, 1)
@@ -19,14 +18,14 @@ namespace Rocks
 			: this(method, 1)
 		{ }
 
-		public HandlerInformation(uint expectedCallCount)
-			: this(null, expectedCallCount)
+		public HandlerInformation(uint ExpectedCallCount)
+			: this(null, ExpectedCallCount)
 		{ }
 
 		public HandlerInformation(Delegate method, uint expectedCallCount)
 		{
 			this.Method = method;
-			this.expectedCallCount = expectedCallCount;
+			this.ExpectedCallCount = expectedCallCount;
 		}
 
 		public void IncrementCallCount()
@@ -38,10 +37,10 @@ namespace Rocks
 		{
 			var verifications = new List<string>();
 
-			if(this.expectedCallCount != this.callCount)
+			if(this.ExpectedCallCount != this.callCount)
 			{
 				verifications.Add(string.Format(HandlerInformation.ErrorExpectedCallCount,
-					this.expectedCallCount, this.callCount));
+					this.ExpectedCallCount, this.callCount));
 			}
 
 			return verifications.AsReadOnly();
@@ -52,6 +51,7 @@ namespace Rocks
 			get { return this.callCount; }
 		}
 
+		public uint ExpectedCallCount { get; private set; }
 		public Delegate Method { get; private set; }
 	}
 }
