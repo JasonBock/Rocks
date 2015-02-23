e40fa71d4746bab9b36798132f9f488f8a72662a
Jason Bock <jason@jasonbock.net>
2015-02-23 15:08:50 -0600

    Started work on argument validation
    Basically a way to specify what a value should be in a method argument

---
+++ Rocks.Tests/ArgumentExpectationTests.cs
@@ -0,0 +1,64 @@
+﻿using NUnit.Framework;
+using System;
+using System.Linq.Expressions;
+
+namespace Rocks.Tests
+{
+	[TestFixture]
+	public sealed class ArgumentExpectationTests
+	{
+		[Test]
+		public void Create()
+		{
+			var expectation = new ArgumentExpectation<int>();
+			Assert.IsTrue(expectation.IsValid(1), nameof(expectation.IsValid));
+		}
+
+		[Test]
+		public void CreateWithNullValue()
+		{
+			Assert.Throws<ArgumentNullException>(() => new ArgumentExpectation<string>(null as string));
+		}
+
+		[Test]
+		public void CreateWithInvalidValue()
+		{
+			var expectation = new ArgumentExpectation<string>("a");
+			Assert.IsFalse(expectation.IsValid("b"), nameof(expectation.IsValid));
+		}
+
+		[Test]
+		public void CreateWithValidValue()
+		{
+			var expectation = new ArgumentExpectation<string>("a");
+			Assert.IsTrue(expectation.IsValid("a"), nameof(expectation.IsValid));
+		}
+
+		[Test]
+		public void CreateWithNullExpress()
+		{
+			Assert.Throws<ArgumentNullException>(() => new ArgumentExpectation<string>(null as Expression));
+		}
+
+		[Test]
+		public void CreateWithInvalidValueFromExpression()
+		{
+			var expression = Expression.Call(this.GetType().GetMethod(nameof(ArgumentExpectationTests.GetValue)));
+			var expectation = new ArgumentExpectation<string>(expression);
+			Assert.IsFalse(expectation.IsValid("b"), nameof(expectation.IsValid));
+		}
+
+		[Test]
+		public void CreateWithValidValueFromExpression()
+		{
+			var expression = Expression.Call(this.GetType().GetMethod(nameof(ArgumentExpectationTests.GetValue)));
+			var expectation = new ArgumentExpectation<string>(expression);
+			Assert.IsTrue(expectation.IsValid("a"), nameof(expectation.IsValid));
+		}
+
+		public static string GetValue()
+		{
+			return "a";
+		}
+	}
+}
---
+++ Rocks/ArgumentExpectation.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Linq.Expressions;
+
+namespace Rocks
+{
+	internal sealed class ArgumentExpectation<T>
+	{
+		private bool isAny;
+		private bool isValue;
+		private Delegate expression;
+		private T value = default(T);
+
+		internal ArgumentExpectation()
+		{
+			this.isAny = true;
+		}
+
+		internal ArgumentExpectation(T value)
+		{
+			if (value == null)
+			{
+				throw new ArgumentNullException(nameof(value));
+			}
+
+			this.isValue = true;
+			this.value = value;
+		}
+
+		internal ArgumentExpectation(Expression expression)
+		{
+			if (expression == null)
+			{
+				throw new ArgumentNullException(nameof(expression));
+			}
+
+			this.expression = Expression.Lambda(expression).Compile();
+		}
+
+		internal bool IsValid(T value)
+		{
+			return this.isAny ? true :
+				this.isValue ? this.value.Equals(value) :
+				((T)this.expression.DynamicInvoke()).Equals(value);
+		}
+	}
+}
