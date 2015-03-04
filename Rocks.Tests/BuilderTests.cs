3eee7a7dd8e88e39f01aac9291a822433b21d7ee
Jason Bock <jason@jasonbock.net>
2015-03-03 20:59:01 -0600

    Added Builder and Compiler Tests
    Self-explanatory

---
+++ Rocks.Tests/BuilderTests.cs
@@ -0,0 +1,36 @@
+﻿using NUnit.Framework;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+
+namespace Rocks.Tests
+{
+	[TestFixture]
+	public sealed class BuilderTests
+	{
+		[Test]
+		public void Build()
+		{
+			var baseType = typeof(IBuilderTest);
+			var handlers = new ReadOnlyDictionary<string, HandlerInformation>(new Dictionary<string, HandlerInformation>());
+			var namespaces = new SortedSet<string> { baseType.Namespace };
+			var options = new Options();
+
+			var builder = new Builder(baseType, handlers, namespaces, options);
+
+			Assert.AreSame(baseType, builder.BaseType, nameof(builder.BaseType));
+			Assert.AreSame(handlers, builder.Handlers, nameof(builder.Handlers));
+			Assert.AreSame(namespaces, builder.Namespaces, nameof(builder.Namespaces));
+			Assert.AreEqual(5, namespaces.Count, nameof(namespaces.Count));
+			Assert.IsTrue(namespaces.Contains("Rocks"), nameof(namespaces));
+			Assert.IsTrue(namespaces.Contains("Rocks.Exceptions"), nameof(namespaces));
+			Assert.IsTrue(namespaces.Contains("Rocks.Tests"), nameof(namespaces));
+			Assert.IsTrue(namespaces.Contains("System"), nameof(namespaces));
+			Assert.IsTrue(namespaces.Contains("System.Collections.ObjectModel"), nameof(namespaces));
+			Assert.AreSame(options, builder.Options, nameof(builder.Options));
+			Assert.IsNotNull(builder.Tree, nameof(builder.Tree));
+			Assert.IsTrue(!string.IsNullOrWhiteSpace(builder.TypeName), nameof(builder.TypeName));
+		}
+	}
+
+	public interface IBuilderTest { }
+}
---
+++ Rocks.Tests/CompilerTests.cs
@@ -0,0 +1,36 @@
+﻿using NUnit.Framework;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+
+namespace Rocks.Tests
+{
+	[TestFixture]
+	public sealed class CompilerTests
+	{
+		[Test]
+		public void Compile()
+		{
+			var baseType = typeof(IBuilderTest);
+			var handlers = new ReadOnlyDictionary<string, HandlerInformation>(new Dictionary<string, HandlerInformation>());
+			var namespaces = new SortedSet<string> { baseType.Namespace };
+			var options = new Options();
+
+			var builder = new Builder(baseType, handlers, namespaces, options);
+
+			var trees = new[] { builder.Tree };
+			var compiler = new Compiler(baseType, trees, options);
+
+			Assert.AreSame(baseType, compiler.BaseType, nameof(compiler.BaseType));
+			Assert.AreSame(options, compiler.Options, nameof(compiler.Options));
+			Assert.AreSame(trees, compiler.Trees, nameof(compiler.Trees));
+			Assert.IsNotNull(compiler.Assembly, nameof(compiler.Assembly));
+			Assert.IsNotNull(
+				(from type in compiler.Assembly.GetTypes()
+				where baseType.IsAssignableFrom(type)
+				select type).Single());
+		}
+	}
+
+	public interface ICompilerTest { }
+}
