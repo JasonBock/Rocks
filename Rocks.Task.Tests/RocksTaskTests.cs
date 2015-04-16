36aac4500a82576266e2020b3368c187dce80b8e
Jason Bock <jason@jasonbock.net>
2015-04-16 14:24:59 -0500

    Created a MSBuild task to generate a mock assembly.

---
+++ Rocks.Task.Tests/RocksTaskTests.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.Build.Framework;
+using NUnit.Framework;
+using Rocks.RockAssemblyTestContainer;
+
+namespace Rocks.Task.Tests
+{
+	[TestFixture]
+	public sealed class RocksTaskTests
+	{
+		[Test]
+		public void Create()
+		{
+			var engineMock = Rock.Create<IBuildEngine>();
+			engineMock.HandleAction(_ => _.LogMessageEvent(Arg.IsAny<BuildMessageEventArgs>()), 2);
+
+			var task = new RocksTask { AssemblyLocation = typeof(Class1).Assembly.Location };
+			task.BuildEngine = engineMock.Make();
+			task.Execute();
+
+			Assert.IsNotNull(task.Result);
+
+			engineMock.Verify();
+		}
+	}
+}
---
+++ Rocks.Task/RocksTask.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.Build.Framework;
+using Microsoft.Build.Utilities;
+using System.Diagnostics;
+using System.Reflection;
+
+namespace Rocks.Task
+{
+	public sealed class RocksTask
+		: AppDomainIsolatedTask
+	{
+		public override bool Execute()
+		{
+			this.Log.LogMessage($"Generating mocks for assembly {this.AssemblyLocation}...");
+			var stopwatch = Stopwatch.StartNew();
+			this.Result = new RockAssembly(Assembly.LoadFile(this.AssemblyLocation)).Result;
+			stopwatch.Stop();
+			this.Log.LogMessage($"Generating mocks for {this.AssemblyLocation} complete, results are in {this.Result.FullName} - total time: {stopwatch.Elapsed}.");
+			return true;
+		}
+
+		public Assembly Result { get; private set; }
+
+		[Required]
+		public string AssemblyLocation { get; set; }
+	}
+}
