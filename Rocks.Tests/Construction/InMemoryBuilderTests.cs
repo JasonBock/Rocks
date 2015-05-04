1017d74ac09479af02e44353f1a0ec9c2ae2497c
Jason Bock <jason@jasonbock.net>
2015-05-03 20:32:01 -0500

    Oops! Fixed a broken test

--- Rocks.Tests/Construction/InMemoryBuilderTests.cs
+++ Rocks.Tests/Construction/InMemoryBuilderTests.cs
@@ -27,10 +27,9 @@ namespace Rocks.Tests.Construction
 			Assert.AreSame(baseType, builder.BaseType, nameof(builder.BaseType));
 			Assert.AreSame(handlers, builder.Handlers, nameof(builder.Handlers));
 			Assert.AreSame(namespaces, builder.Namespaces, nameof(builder.Namespaces));
-			Assert.AreEqual(6, namespaces.Count, nameof(namespaces.Count));
+			Assert.AreEqual(5, namespaces.Count, nameof(namespaces.Count));
 			Assert.IsTrue(namespaces.Contains(typeof(Rock).Namespace), nameof(namespaces));
 			Assert.IsTrue(namespaces.Contains(typeof(CompilationException).Namespace), nameof(namespaces));
-			Assert.IsTrue(namespaces.Contains(typeof(IBuilderTest).Namespace), nameof(namespaces));
 			Assert.IsTrue(namespaces.Contains(typeof(object).Namespace), nameof(namespaces));
 			Assert.IsTrue(namespaces.Contains(typeof(ReadOnlyDictionary<,>).Namespace), nameof(namespaces));
 			Assert.IsTrue(namespaces.Contains(typeof(BindingFlags).Namespace), nameof(namespaces));
