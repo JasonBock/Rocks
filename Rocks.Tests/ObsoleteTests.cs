2616215ed709e75846db5860095b6baf450b4509
Jason Bock <jason@jasonbock.net>
2015-08-04 09:03:03 -0500

    #4 ensuring obsolete members don't cause issues with mock generation

---
+++ Rocks.Tests/ObsoleteTests.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.CodeAnalysis;
+using NUnit.Framework;
+using Rocks.Construction;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Reflection;
+
+namespace Rocks.Tests
+{
+	[TestFixture]
+	public sealed class ObsoleteTests
+	{
+		[Test]
+		public void CreateMockWithObsoleteMemberWithIsErrorSetToTrue()
+		{
+			var rock = Rock.Create<IHaveAnObsoleteMember>();
+			rock.Handle(_ => _.Method());
+
+			var mock = rock.Make();
+			mock.Method();
+
+			rock.Verify();
+		}
+	}
+
+	public interface IHaveAnObsoleteMember
+	{
+		[Obsolete("a", true)]
+		void ObsoleteMethod();
+
+		void Method();
+	}
+}
