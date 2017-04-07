7dcb5f62e9be1e0548f3b620720a9f68eb0e6301
Jason Bock <jason@jasonbock.net>
2017-04-07 08:18:32 -0500

    #40 simple example works now in .NET Core.
    Moving on to tests....

--- Rocks.Shared/Construction/Compiler.cs
+++ Rocks.Shared/Construction/Compiler.cs
@@ -11,7 +11,52 @@ using System.Reflection;
 
 namespace Rocks.Construction
 {
+	internal abstract class Compiler
+	{
+#if NETCOREAPP1_1
+		// Lifted from:
+		// https://github.com/dotnet/orleans/blob/master/src/OrleansCodeGenerator/CodeGeneratorCommon.cs#L65
+		// https://github.com/dotnet/orleans/blob/master/vNext/src/Orleans/Shims/AppDomain.cs
+		protected static Lazy<HashSet<Assembly>> assemblyReferences =
+			new Lazy<HashSet<Assembly>>(() =>
+			{
+				void LoadDependencies(HashSet<Assembly> loadedAssemblies, Assembly fromAssembly)
+				{
+					foreach (var reference in fromAssembly.GetReferencedAssemblies())
+					{
+						var assembly = Assembly.Load(reference);
+
+						if (loadedAssemblies.Add(assembly))
+						{
+							LoadDependencies(loadedAssemblies, assembly);
+						}
+					}
+				}
+
+				var baseDirectory = AppContext.BaseDirectory;
+				var assemblyFiles = Directory.EnumerateFiles(baseDirectory, "*.dll");
+				var assemblyNames = assemblyFiles.Select(Path.GetFileNameWithoutExtension).ToList();
+				var assemblies = new HashSet<Assembly>();
+
+				foreach (var assemblyName in assemblyNames)
+				{
+					assemblies.Add(Assembly.Load(new AssemblyName(assemblyName)));
+				}
+
+				assemblies.Add(typeof(Exception).GetTypeInfo().Assembly);
+
+				foreach (var assembly in assemblies.ToList())
+				{
+					LoadDependencies(assemblies, assembly);
+				}
+
+				return assemblies;
+			});
+#endif
+	}
+
 	internal abstract class Compiler<T>
+		: Compiler
 		where T : Stream
 	{
 		protected Compiler(IEnumerable<SyntaxTree> trees, OptimizationSetting optimization, string assemblyName,
@@ -43,6 +88,11 @@ namespace Rocks.Construction
 			// we need to pass BinderFlags.IgnoreCorLibraryDuplicatedTypes, but it's an internal class 
 			// http://source.roslyn.io/#Microsoft.CodeAnalysis.CSharp/Binder/BinderFlags.cs,00f268571bb66b73 
 			options = (CSharpCompilationOptions)method.Invoke(options, new object[] { 1u << 26 });
+
+			options = options.WithSpecificDiagnosticOptions(new Dictionary<string, ReportDiagnostic>
+			{
+				{ "CS1702", ReportDiagnostic.Hidden }
+			});
 #endif
 
 			var compilation = CSharpCompilation.Create(this.AssemblyName,
@@ -61,7 +111,7 @@ namespace Rocks.Construction
 						results.Diagnostics.Length > 0 &&
 						results.Diagnostics.Where(_ => _.Severity == DiagnosticSeverity.Hidden).ToArray().Length != results.Diagnostics.Length))
 				{
-					//throw new CompilationException(results.Diagnostics);
+					throw new CompilationException(results.Diagnostics);
 				}
 
 				this.ProcessStreams(assemblyStream, pdbStream);
@@ -84,80 +134,16 @@ namespace Rocks.Construction
 			return references.ToArray();
 		}
 #else
-		private static HashSet<Assembly> GetAssembliesList()
-		{
-			var path = AppContext.BaseDirectory;
-			var assemblyFiles = Directory.EnumerateFiles(path, "*.dll");
-			var assemblyNames = assemblyFiles.Select(Path.GetFileNameWithoutExtension).ToList();
-			var assemblies = new HashSet<Assembly>();
-
-			foreach (var assemblyName in assemblyNames)
-			{
-				assemblies.Add(Assembly.Load(new AssemblyName(assemblyName)));
-			}
-
-			assemblies.Add(typeof(Exception).GetTypeInfo().Assembly);
-
-			foreach (var assembly in assemblies.ToList())
-			{
-				LoadDependencies(assemblies, assembly);
-			}
-
-			return assemblies;
-		}
-
-		private static void LoadDependencies(HashSet<Assembly> loadedAssemblies, Assembly fromAssembly)
-		{
-			foreach (var reference in fromAssembly.GetReferencedAssemblies())
-			{
-				var asm = Assembly.Load(reference);
-				if (loadedAssemblies.Add(asm))
-				{
-					LoadDependencies(loadedAssemblies, asm);
-				}
-			}
-		}
-
 		private MetadataReference[] GetReferences()
 		{
-			// TODO: Should seriously give this a try:
-			// https://github.com/dotnet/orleans/blob/master/src/OrleansCodeGenerator/CodeGeneratorCommon.cs#L65
-			// https://github.com/dotnet/orleans/blob/master/vNext/src/Orleans/Shims/AppDomain.cs
-			return GetAssembliesList()
-				.Where(asm => !asm.IsDynamic && !string.IsNullOrWhiteSpace(asm.Location))
-				.Select(asm => MetadataReference.CreateFromFile(asm.Location))
+			var references = Compiler.assemblyReferences.Value;
+			this.ReferencedAssemblies.ToList().ForEach(_ => references.Add(_));
+
+			return references
+				.Where(_ => !_.IsDynamic && !string.IsNullOrWhiteSpace(_.Location))
+				.Select(_ => MetadataReference.CreateFromFile(_.Location))
 				.Cast<MetadataReference>()
 				.ToArray();
-
-			// Lifted from:
-			// http://stackoverflow.com/questions/38943899/net-core-cs0012-object-is-defined-in-an-assembly-that-is-not-referenced
-			// http://stackoverflow.com/questions/39257074/net-core-amd-roslyn-csharpcompilation-the-type-object-is-defined-in-an-assem
-			//var netCoreAppLocationDirectory = Path.GetDirectoryName(typeof(object).GetTypeInfo().Assembly.Location);
-
-			//var assemblyPaths = new HashSet<string>(
-			//	this.ReferencedAssemblies.Select(_ => _.Location));
-			//assemblyPaths.Add(typeof(object).GetTypeInfo().Assembly.Location);
-			//assemblyPaths.Add(typeof(IMock).GetTypeInfo().Assembly.Location);
-			////assemblyPaths.Add(typeof(Action<,,,,,,,,>).GetTypeInfo().Assembly.Location);
-			//assemblyPaths.Add(Path.Combine(netCoreAppLocationDirectory, "mscorlib.dll"));
-			////assemblyPaths.Add(Path.Combine(netCoreAppLocationDirectory, "System.ObjectModel.dll"));
-			//assemblyPaths.Add(Path.Combine(netCoreAppLocationDirectory, "System.Runtime.dll"));
-
-			//return assemblyPaths.Select(_ => MetadataReference.CreateFromFile(_)).ToArray();
-
-			//var references = new List<MetadataReference>(
-			//	this.ReferencedAssemblies.Select(_ => MetadataReference.CreateFromFile(_.Location)));
-			//var netCoreAppLocationDirectory = Directory.GetParent(typeof(object).GetTypeInfo().Assembly.Location);
-			//references.AddRange(new[]
-			//	{
-			//		MetadataReference.CreateFromFile(Path.Combine(netCoreAppLocationDirectory.FullName, "mscorlib.dll")),
-			//		MetadataReference.CreateFromFile(Path.Combine(netCoreAppLocationDirectory.FullName, "System.ObjectModel.dll")),
-			//		MetadataReference.CreateFromFile(typeof(object).GetTypeInfo().Assembly.Location),
-			//		MetadataReference.CreateFromFile(typeof(IMock).GetTypeInfo().Assembly.Location),
-			//		MetadataReference.CreateFromFile(typeof(Action<,,,,,,,,>).GetTypeInfo().Assembly.Location),
-			//		MetadataReference.CreateFromFile(typeof(ReadOnlyDictionary<,>).GetTypeInfo().Assembly.Location)
-			//	});
-			//return references.ToArray();
 		}
 #endif
 		protected abstract T GetAssemblyStream();
