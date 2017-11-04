8470e34e2d897ed5d10af7282382b8be5aa13c91
Jason Bock <jason@jasonbock.net>
2017-11-04 16:42:09 -0500

    Updated for 3.1.0 publish

--- Rocks.Sketchpad/Program.cs
+++ Rocks.Sketchpad/Program.cs
@@ -14,16 +14,18 @@ namespace Rocks.Sketchpad
 	{
 		static void Main(string[] args)
 		{
-			// Use expression body for methods
-			//await EvaluateExpressionAsync("Do()");
-			//await EvaluateExpressionAsync("Do(");
-			//await EvaluateExpressionAsync("Do(3, 4)");
-			//await EvaluateExpressionAsync("Do(3, \"hi\", 'c', 4)");
-			//await EvaluateExpressionAsync("Do(3, Guid.NewGuid())");
-			//await EvaluateExpressionAsync("Do(3, Guid.Parse(Guid.NewGuid().ToString(\"N\")))");
-			//var rock = Rock.Create<IStupid>();
-			//rock.Handle(_ => _.Foo());
-			//var chunk = rock.Make();
+#pragma warning disable IDE0022 // Use expression body for methods
+			BenchmarkRunner.Run<MetadataReferenceCacheBenchmark>();
+#pragma warning restore IDE0022 // Use expression body for methods
+										 //await EvaluateExpressionAsync("Do()");
+										 //await EvaluateExpressionAsync("Do(");
+										 //await EvaluateExpressionAsync("Do(3, 4)");
+										 //await EvaluateExpressionAsync("Do(3, \"hi\", 'c', 4)");
+										 //await EvaluateExpressionAsync("Do(3, Guid.NewGuid())");
+										 //await EvaluateExpressionAsync("Do(3, Guid.Parse(Guid.NewGuid().ToString(\"N\")))");
+										 //var rock = Rock.Create<IStupid>();
+										 //rock.Handle(_ => _.Foo());
+										 //var chunk = rock.Make();
 		}
 
 		private static void RunBenchmark() =>
