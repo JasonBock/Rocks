740f98eb2a811e60aa24c73b33469278690b3ba3
Jason Bock <jason@jasonbock.net>
2019-01-04 07:49:10 -0600

    Removed some usings

--- Rocks/MethodAdornmentsOfTResult.cs
+++ Rocks/MethodAdornmentsOfTResult.cs
@@ -1,6 +1,4 @@
-﻿using System;
-
-namespace Rocks
+﻿namespace Rocks
 {
 	public sealed class MethodAdornments<TResult>
 		: MethodAdornments
--- Rocks/Templates/MethodTemplates.cs
+++ Rocks/Templates/MethodTemplates.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.Reflection;
 using System.Threading.Tasks;
 using static Rocks.Extensions.TypeExtensions;
 
