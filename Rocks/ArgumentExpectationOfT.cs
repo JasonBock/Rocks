40796efc349da5b2d6537cbbdd6b3035d8239715
Jason Bock <jason@jasonbock.net>
2020-04-27 08:31:55 -0500

    #119 made some progress on addressing [AllowNull] issues, still needs more work

--- Rocks/ArgumentExpectationOfT.cs
+++ Rocks/ArgumentExpectationOfT.cs
@@ -1,11 +1,21 @@
 ﻿using System;
+using System.Diagnostics.CodeAnalysis;
 
 namespace Rocks
 {
 	[Serializable]
-   public abstract class ArgumentExpectation<T>
+	public abstract class ArgumentExpectation<T>
 		: ArgumentExpectation
-   {
-	  public abstract bool IsValid(T value);
-   }
+	{
+		public abstract bool IsValid([AllowNull] T value);
+	}
+
+
+	public class A<T>
+	{
+		public bool Equals([AllowNull] T x, [AllowNull] T y) => 
+			new Func<T, T, bool>(this.Compare)(x, y);
+
+		public bool Compare(T a, T b) => true;
+	}
 }
