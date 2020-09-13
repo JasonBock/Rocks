b20e9b54398ce19d565af9e3cfe3c0c431fc1cc6
Jason Bock <jason@jasonbock.net>
2020-09-12 23:58:41 -0500

    #121 even more extension method tests

--- Rocks.Generators.Tests/Extensions/ISymbolExtensionsCanBeSeenByContainingAssemblyTests.cs
+++ Rocks.Generators.Tests/Extensions/ISymbolExtensionsCanBeSeenByContainingAssemblyTests.cs
@@ -29,49 +29,186 @@ namespace Rocks.Tests.Extensions
 		[Test]
 		public static void CallWhenSymbolIsProtected()
 		{
+			var code =
+@"public class Source
+{
+	protected void Foo() { }
+}";
+			var symbol = ISymbolExtensionsCanBeSeenByContainingAssemblyTests.GetSymbol(code);
 
+			Assert.Multiple(() =>
+			{
+				Assert.That(symbol.CanBeSeenByContainingAssembly(symbol.ContainingAssembly), Is.True);
+			});
 		}
 
 		[Test]
 		public static void CallWhenSymbolIsProtectedInternal()
 		{
+			var code =
+@"public class Source
+{
+	protected internal void Foo() { }
+}";
+			var symbol = ISymbolExtensionsCanBeSeenByContainingAssemblyTests.GetSymbol(code);
 
+			Assert.Multiple(() =>
+			{
+				Assert.That(symbol.CanBeSeenByContainingAssembly(symbol.ContainingAssembly), Is.True);
+			});
 		}
 
 		[Test]
 		public static void CallWhenSymbolIsInternalAndContainingAssemblyEqualsInvocationAssembly()
 		{
+			var code =
+@"public class Source
+{
+	internal void Foo() { }
+}";
+			var symbol = ISymbolExtensionsCanBeSeenByContainingAssemblyTests.GetSymbol(code);
 
+			Assert.Multiple(() =>
+			{
+				Assert.That(symbol.CanBeSeenByContainingAssembly(symbol.ContainingAssembly), Is.True);
+			});
 		}
 
 		[Test]
 		public static void CallWhenSymbolIsInternalAndContainingAssemblyExposesToInvocationAssembly()
 		{
+			const string ContainingAssembly = nameof(ContainingAssembly);
+			var code =
+@$"using System.Runtime.CompilerServices;
 
+[assembly: InternalsVisibleTo(""{ContainingAssembly}"")]
+
+public class Source
+{{
+	internal void Foo() {{ }}
+}}";
+			var symbol = ISymbolExtensionsCanBeSeenByContainingAssemblyTests.GetSymbol(code);
+
+			var containingSyntaxTree = CSharpSyntaxTree.ParseText("public class Containing { }");
+			var containingReferences = AppDomain.CurrentDomain.GetAssemblies()
+				.Where(_ => !_.IsDynamic && !string.IsNullOrWhiteSpace(_.Location))
+				.Select(_ =>
+				{
+					var location = _.Location;
+					return MetadataReference.CreateFromFile(location);
+				});
+			var containingCompilation = CSharpCompilation.Create(ContainingAssembly, new SyntaxTree[] { containingSyntaxTree },
+				containingReferences, new CSharpCompilationOptions(OutputKind.DynamicallyLinkedLibrary));
+
+			Assert.Multiple(() =>
+			{
+				Assert.That(symbol.CanBeSeenByContainingAssembly(containingCompilation.Assembly), Is.True);
+			});
 		}
 
 		[Test]
 		public static void CallWhenSymbolIsInternalAndContainingAssemblyDoesNotExposeToInvocationAssembly()
 		{
+			const string ContainingAssembly = nameof(ContainingAssembly);
+			var code =
+@$"public class Source
+{{
+	internal void Foo() {{ }}
+}}";
+			var symbol = ISymbolExtensionsCanBeSeenByContainingAssemblyTests.GetSymbol(code);
 
+			var containingSyntaxTree = CSharpSyntaxTree.ParseText("public class Containing { }");
+			var containingReferences = AppDomain.CurrentDomain.GetAssemblies()
+				.Where(_ => !_.IsDynamic && !string.IsNullOrWhiteSpace(_.Location))
+				.Select(_ =>
+				{
+					var location = _.Location;
+					return MetadataReference.CreateFromFile(location);
+				});
+			var containingCompilation = CSharpCompilation.Create(ContainingAssembly, new SyntaxTree[] { containingSyntaxTree },
+				containingReferences, new CSharpCompilationOptions(OutputKind.DynamicallyLinkedLibrary));
+
+			Assert.Multiple(() =>
+			{
+				Assert.That(symbol.CanBeSeenByContainingAssembly(containingCompilation.Assembly), Is.False);
+			});
 		}
 
 		[Test]
-		public static void CallWhenSymbolIsProtectedInternalAndContainingAssemblyEqualsInvocationAssembly()
+		public static void CallWhenSymbolIsPrivateProtectedAndContainingAssemblyEqualsInvocationAssembly()
 		{
+			const string ContainingAssembly = nameof(ContainingAssembly);
+			var code =
+@$"public class Source
+{{
+	private protected void Foo() {{ }}
+}}";
+			var symbol = ISymbolExtensionsCanBeSeenByContainingAssemblyTests.GetSymbol(code);
 
+			Assert.Multiple(() =>
+			{
+				Assert.That(symbol.CanBeSeenByContainingAssembly(symbol.ContainingAssembly), Is.True);
+			});
 		}
 
 		[Test]
-		public static void CallWhenSymbolIsProtectedInternalAndContainingAssemblyExposesToInvocationAssembly()
+		public static void CallWhenSymbolIsPrivateProtectedAndContainingAssemblyExposesToInvocationAssembly()
 		{
+			const string ContainingAssembly = nameof(ContainingAssembly);
+			var code =
+@$"using System.Runtime.CompilerServices;
 
+[assembly: InternalsVisibleTo(""{ContainingAssembly}"")]
+
+public class Source
+{{
+	private protected void Foo() {{ }}
+}}";
+			var symbol = ISymbolExtensionsCanBeSeenByContainingAssemblyTests.GetSymbol(code);
+
+			var containingSyntaxTree = CSharpSyntaxTree.ParseText("public class Containing { }");
+			var containingReferences = AppDomain.CurrentDomain.GetAssemblies()
+				.Where(_ => !_.IsDynamic && !string.IsNullOrWhiteSpace(_.Location))
+				.Select(_ =>
+				{
+					var location = _.Location;
+					return MetadataReference.CreateFromFile(location);
+				});
+			var containingCompilation = CSharpCompilation.Create(ContainingAssembly, new SyntaxTree[] { containingSyntaxTree },
+				containingReferences, new CSharpCompilationOptions(OutputKind.DynamicallyLinkedLibrary));
+
+			Assert.Multiple(() =>
+			{
+				Assert.That(symbol.CanBeSeenByContainingAssembly(containingCompilation.Assembly), Is.True);
+			});
 		}
 
 		[Test]
-		public static void CallWhenSymbolIsProtectedInternalAndContainingAssemblyDoesNotExposeToInvocationAssembly()
+		public static void CallWhenSymbolIsPrivateProtectedAndContainingAssemblyDoesNotExposeToInvocationAssembly()
 		{
+			const string ContainingAssembly = nameof(ContainingAssembly);
+			var code =
+@"public class Source
+{
+	private protected void Foo() { }
+}";
+			var symbol = ISymbolExtensionsCanBeSeenByContainingAssemblyTests.GetSymbol(code);
 
+			var containingSyntaxTree = CSharpSyntaxTree.ParseText("public class Containing { }");
+			var containingReferences = AppDomain.CurrentDomain.GetAssemblies()
+				.Where(_ => !_.IsDynamic && !string.IsNullOrWhiteSpace(_.Location))
+				.Select(_ =>
+				{
+					var location = _.Location;
+					return MetadataReference.CreateFromFile(location);
+				});
+			var containingCompilation = CSharpCompilation.Create(ContainingAssembly, new SyntaxTree[] { containingSyntaxTree },
+				containingReferences, new CSharpCompilationOptions(OutputKind.DynamicallyLinkedLibrary));
+
+			Assert.Multiple(() =>
+			{
+				Assert.That(symbol.CanBeSeenByContainingAssembly(containingCompilation.Assembly), Is.False);
+			});
 		}
 
 		private static ISymbol GetSymbol(string source)
--- Rocks.Generators/Extensions/ISymbolExtensions.cs
+++ Rocks.Generators/Extensions/ISymbolExtensions.cs
@@ -6,6 +6,10 @@ namespace Rocks.Extensions
 	{
 		internal static bool CanBeSeenByContainingAssembly(this ISymbol self, IAssemblySymbol containingAssemblyOfInvocationSymbol)
 		{
+			/*
+			protected internal -> ProtectedOrInternal
+			private protected -> ProtectedAndInternal
+			*/
 			if (self.DeclaredAccessibility == Accessibility.Public ||
 				self.DeclaredAccessibility == Accessibility.Protected ||
 				self.DeclaredAccessibility == Accessibility.ProtectedOrInternal)
