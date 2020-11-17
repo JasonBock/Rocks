6d10dc7516d187c7f75a8feaf04a1e3d11344053
Jason Bock <jason.r.bock@outlook.com>
2020-11-17 14:17:05 -0600

    #121 even more tests

---
+++ src/Rocks.Generators.Tests/Extensions/TypeExtensionsTests.cs
@@ -0,0 +1,40 @@
+﻿using NUnit.Framework;
+using Rocks.Extensions;
+
+namespace Rocks.Tests.Extensions
+{
+	public static class Identifiers
+	{
+		[MemberIdentifier(1, "Foo")]
+		public static void Foo() { }
+	}
+
+	public static class NoIdentifiers
+	{
+		public static void Foo() { }
+	}
+
+	public static class TypeExtensionsTests
+	{
+		[Test]
+		public static void GetMemberDescription() =>
+			Assert.Multiple(() =>
+			{
+				Assert.That(typeof(Identifiers).GetMemberDescription(1), Is.EqualTo("Foo"));
+			});
+
+		[Test]
+		public static void GetMemberDescriptionWhenIdentifierIsNotFound() =>
+			Assert.Multiple(() =>
+			{
+				Assert.That(typeof(Identifiers).GetMemberDescription(2), Is.Null);
+			});
+
+		[Test]
+		public static void GetMemberDescriptionWhenAttributesDoNotExist() =>
+			Assert.Multiple(() =>
+			{
+				Assert.That(typeof(NoIdentifiers).GetMemberDescription(1), Is.Null);
+			});
+	}
+}
