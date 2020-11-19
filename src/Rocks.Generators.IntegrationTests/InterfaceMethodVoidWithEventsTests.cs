8ce054382bcd6196929119ba06757332ad5a9a8f
Jason Bock <jason.r.bock@outlook.com>
2020-11-19 09:18:42 -0600

    #121 interface method void with events tests done

---
+++ src/Rocks.Generators.IntegrationTests/InterfaceMethodVoidWithEventsTests.cs
@@ -0,0 +1,103 @@
+﻿using NUnit.Framework;
+using System;
+
+namespace Rocks.IntegrationTests
+{
+	public interface IInterfaceMethodVoidWithEventsTests
+	{
+		void NoParameters();
+		event EventHandler MyEvent;
+	}
+
+	public static class InterfaceMethodVoidWithEventsTests
+	{
+		[Test]
+		public static void RaiseEvent()
+		{
+			var rock = Rock.Create<IInterfaceMethodVoidWithEventsTests>();
+			rock.Methods().NoParameters().RaisesMyEvent(EventArgs.Empty);
+
+			var wasEventRaised = false;
+			var chunk = rock.Instance();
+			chunk.MyEvent += (s, e) => wasEventRaised = true;
+			chunk.NoParameters();
+
+			rock.Verify();
+
+			Assert.Multiple(() =>
+			{
+				Assert.That(wasEventRaised, Is.True);
+			});
+		}
+
+		[Test]
+		public static void RaiseEventWithCallback()
+		{
+			var wasCallbackInvoked = false;
+			var rock = Rock.Create<IInterfaceMethodVoidWithEventsTests>();
+			rock.Methods().NoParameters()
+				.Callback(() => wasCallbackInvoked = true)
+				.RaisesMyEvent(EventArgs.Empty);
+
+			var wasEventRaised = false;
+			var chunk = rock.Instance();
+			chunk.MyEvent += (s, e) => wasEventRaised = true;
+			chunk.NoParameters();
+
+			rock.Verify();
+
+			Assert.Multiple(() =>
+			{
+				Assert.That(wasEventRaised, Is.True);
+				Assert.That(wasCallbackInvoked, Is.True);
+			});
+		}
+
+		[Test]
+		public static void RaiseEventWithMultipleCalls()
+		{
+			var rock = Rock.Create<IInterfaceMethodVoidWithEventsTests>();
+			rock.Methods().NoParameters()
+				.CallCount(2)
+				.RaisesMyEvent(EventArgs.Empty);
+
+			var eventRaisedCount = 0;
+			var chunk = rock.Instance();
+			chunk.MyEvent += (s, e) => eventRaisedCount++;
+			chunk.NoParameters();
+			chunk.NoParameters();
+
+			rock.Verify();
+
+			Assert.Multiple(() =>
+			{
+				Assert.That(eventRaisedCount, Is.EqualTo(2));
+			});
+		}
+
+		[Test]
+		public static void RaiseEventWithMultipleCallsWithCallback()
+		{
+			var callbackInvokedCount = 0;
+			var rock = Rock.Create<IInterfaceMethodVoidWithEventsTests>();
+			rock.Methods().NoParameters()
+				.CallCount(2)
+				.Callback(() => callbackInvokedCount++)
+				.RaisesMyEvent(EventArgs.Empty);
+
+			var eventRaisedCount = 0;
+			var chunk = rock.Instance();
+			chunk.MyEvent += (s, e) => eventRaisedCount++;
+			chunk.NoParameters();
+			chunk.NoParameters();
+
+			rock.Verify();
+
+			Assert.Multiple(() =>
+			{
+				Assert.That(eventRaisedCount, Is.EqualTo(2));
+				Assert.That(callbackInvokedCount, Is.EqualTo(2));
+			});
+		}
+	}
+}
