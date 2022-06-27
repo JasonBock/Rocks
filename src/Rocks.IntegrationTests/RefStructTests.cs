39234ba54f6cafbc561cdfcf85830b19031f88d1
Jason Bock <jason.r.bock@outlook.com>
2022-06-26 21:33:43 -0500

    #167 YAY! All code generation errors are gone

--- src/Rocks.IntegrationTests/RefStructTests.cs
+++ src/Rocks.IntegrationTests/RefStructTests.cs
@@ -5,7 +5,7 @@ namespace Rocks.IntegrationTests;
 public interface IReturnSpan
 {
 	Span<int> GetRandomData();
-	Span<byte> Values { get; set; }
+	Span<byte> Values { get; }
 }
 
 public interface IHaveSpan
@@ -16,6 +16,41 @@ public interface IHaveSpan
 
 public static class RefStructTests
 {
+	[Test]
+	public static void CreateWithReturnValues()
+	{
+		var rock = Rock.Create<IReturnSpan>();
+		rock.Methods().GetRandomData().Returns(() => new[] { 1, 2 }.AsSpan());
+		rock.Properties().Getters().Values().Returns(() => new byte[] { 3, 4, 5 }.AsSpan());
+
+		var chunk = rock.Instance();
+
+		Assert.Multiple(() =>
+		{
+			var data = chunk.GetRandomData();
+			Assert.That(data.Length, Is.EqualTo(2));
+			Assert.That(data[0], Is.EqualTo(1));
+			Assert.That(data[1], Is.EqualTo(2));
+			var values = chunk.Values;
+			Assert.That(values.Length, Is.EqualTo(3));
+			Assert.That(values[0], Is.EqualTo(3));
+			Assert.That(values[1], Is.EqualTo(4));
+			Assert.That(values[2], Is.EqualTo(5));
+		});
+
+		rock.Verify();
+	}
+
+	[Test]
+	public static void MakeWithReturnValues()
+	{
+		var chunk = Rock.Make<IReturnSpan>().Instance();
+#pragma warning disable NUnit2010 // Use EqualConstraint for better assertion messages in case of failure
+		Assert.That(chunk.GetRandomData() == default(Span<int>), Is.True);
+		Assert.That(chunk.Values == default(Span<byte>), Is.True);
+#pragma warning restore NUnit2010 // Use EqualConstraint for better assertion messages in case of failure
+	}
+
 	[Test]
 	public static void CreateWithSpanOfInt()
 	{
