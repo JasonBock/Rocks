7ad886a192dfb8b02669bee1b6b6dfd47920f13f
Jason Bock <jason.r.bock@outlook.com>
2022-09-26 07:32:11 -0500

    #129 Rock.Make() tests pass

--- src/Rocks.Tests/RockMakeGeneratorTests.cs
+++ src/Rocks.Tests/RockMakeGeneratorTests.cs
@@ -11,22 +11,24 @@ public static class RockMakeGeneratorTests
 	public static async Task GenerateWhenInvocationIsNotRockMakeAsync()
 	{
 		var code =
-@"using Rocks;
+			"""
+			using Rocks;
 
-public interface ITest { }
+			public interface ITest { }
 
-public static class Rocker
-{
-	public static void Make<T>() { }
-}
+			public static class Rocker
+			{
+				public static void Make<T>() { }
+			}
 
-public static class Invoker
-{
-	public static void Invoke()
-	{
-		Rocker.Make<ITest>();
-	}
-}";
+			public static class Invoker
+			{
+				public static void Invoke()
+				{
+					Rocker.Make<ITest>();
+				}
+			}
+			""";
 
 		await TestAssistants.RunAsync<RockMakeGenerator>(code,
 			Enumerable.Empty<(Type, string, string)>(),
@@ -37,56 +39,53 @@ public static class Invoker
 	public static async Task GenerateWhenValueTaskOfTIsReturnedAsync()
 	{
 		var code =
-@"using Rocks;
-using System;
-using System.Threading.Tasks;
+			"""
+			using Rocks;
+			using System;
+			using System.Threading.Tasks;
 
-namespace MockTests
-{
-	public static class Test
-	{
-		public static void Generate()
-		{
-			var rock = Rock.Make<ITest>();
-		}
-	}
+			namespace MockTests
+			{
+				public static class Test
+				{
+					public static void Generate()
+					{
+						var rock = Rock.Make<ITest>();
+					}
+				}
 
-	public interface ITest
-	{
-		ValueTask<T> Foo<T>();
-	}
-}";
+				public interface ITest
+				{
+					ValueTask<T> Foo<T>();
+				}
+			}
+			""";
 
 		var generatedCode =
-@"using MockTests;
-using Rocks;
-using Rocks.Exceptions;
-using System;
-using System.Collections.Generic;
-using System.Collections.Immutable;
-using System.Threading.Tasks;
-
-#nullable enable
-namespace MockTests
-{
-	internal static class MakeExpectationsOfITestExtensions
-	{
-		internal static ITest Instance(this MakeGeneration<ITest> self) =>
-			new RockITest();
-		
-		private sealed class RockITest
-			: ITest
-		{
-			public RockITest() { }
+			"""
+			#nullable enable
 			
-			public ValueTask<T> Foo<T>()
+			namespace MockTests
 			{
-				return new ValueTask<T>(default(T)!);
+				internal static class MakeExpectationsOfITestExtensions
+				{
+					internal static global::MockTests.ITest Instance(this global::Rocks.MakeGeneration<global::MockTests.ITest> self) =>
+						new RockITest();
+					
+					private sealed class RockITest
+						: global::MockTests.ITest
+					{
+						public RockITest() { }
+						
+						public global::System.Threading.Tasks.ValueTask<T> Foo<T>()
+						{
+							return new global::System.Threading.Tasks.ValueTask<T>(default(T)!);
+						}
+					}
+				}
 			}
-		}
-	}
-}
-";
+			
+			""";
 
 		await TestAssistants.RunAsync<RockMakeGenerator>(code,
 			new[] { (typeof(RockMakeGenerator), "ITest_Rock_Make.g.cs", generatedCode) },
@@ -97,83 +96,82 @@ namespace MockTests
 	public static async Task GenerateWhenTargetTypeIsValidAsync()
 	{
 		var code =
-@"using Rocks;
-using System;
-using System.Threading.Tasks;
+			"""
+			using Rocks;
+			using System;
+			using System.Threading.Tasks;
 
-namespace MockTests
-{
-	public interface ITest
-	{
-		unsafe int* Foo(int* value);
-		unsafe delegate*<int, void> FooMethod(delegate*<int, void> value);
-		unsafe int* Data { get; set; }
-		unsafe delegate*<int, void> DataMethod { get; set; }
-		unsafe int* this[int* a] { get; set; }
-		unsafe delegate*<int, void> this[delegate*<int, void> a] { get; set; }
-	}
+			namespace MockTests
+			{
+				public interface ITest
+				{
+					unsafe int* Foo(int* value);
+					unsafe delegate*<int, void> FooMethod(delegate*<int, void> value);
+					unsafe int* Data { get; set; }
+					unsafe delegate*<int, void> DataMethod { get; set; }
+					unsafe int* this[int* a] { get; set; }
+					unsafe delegate*<int, void> this[delegate*<int, void> a] { get; set; }
+				}
 
-	public static class Test
-	{
-		public static void Generate()
-		{
-			var rock = Rock.Make<ITest>();
-		}
-	}
-}";
+				public static class Test
+				{
+					public static void Generate()
+					{
+						var rock = Rock.Make<ITest>();
+					}
+				}
+			}
+			""";
 
 		var generatedCode =
-@"using Rocks;
-using Rocks.Exceptions;
-using System;
-using System.Collections.Generic;
-using System.Collections.Immutable;
-
-#nullable enable
-namespace MockTests
-{
-	internal static class MakeExpectationsOfITestExtensions
-	{
-		internal static ITest Instance(this MakeGeneration<ITest> self) =>
-			new RockITest();
-		
-		private sealed class RockITest
-			: ITest
-		{
-			public RockITest() { }
+			"""
+			#nullable enable
 			
-			public unsafe int* Foo(int* value)
+			namespace MockTests
 			{
-				return default!;
+				internal static class MakeExpectationsOfITestExtensions
+				{
+					internal static global::MockTests.ITest Instance(this global::Rocks.MakeGeneration<global::MockTests.ITest> self) =>
+						new RockITest();
+					
+					private sealed class RockITest
+						: global::MockTests.ITest
+					{
+						public RockITest() { }
+						
+						public unsafe int* Foo(int* value)
+						{
+							return default!;
+						}
+						public unsafe delegate*<int, void> FooMethod(delegate*<int, void> value)
+						{
+							return default!;
+						}
+						public unsafe int* Data
+						{
+							get => default!;
+							set { }
+						}
+						public unsafe delegate*<int, void> DataMethod
+						{
+							get => default!;
+							set { }
+						}
+						public unsafe int* this[int* a]
+						{
+							get => default!;
+							set { }
+						}
+						public unsafe delegate*<int, void> this[delegate*<int, void> a]
+						{
+							get => default!;
+							set { }
+						}
+					}
+				}
 			}
-			public unsafe delegate*<int, void> FooMethod(delegate*<int, void> value)
-			{
-				return default!;
-			}
-			public unsafe int* Data
-			{
-				get => default!;
-				set { }
-			}
-			public unsafe delegate*<int, void> DataMethod
-			{
-				get => default!;
-				set { }
-			}
-			public unsafe int* this[int* a]
-			{
-				get => default!;
-				set { }
-			}
-			public unsafe delegate*<int, void> this[delegate*<int, void> a]
-			{
-				get => default!;
-				set { }
-			}
-		}
-	}
-}
-";
+			
+			""";
 
 		await TestAssistants.RunAsync<RockMakeGenerator>(code,
 			new[] { (typeof(RockMakeGenerator), "ITest_Rock_Make.g.cs", generatedCode) },
@@ -184,47 +182,46 @@ namespace MockTests
 	public static async Task GenerateWhenInvocationExistsInTopLevelStatementsAsync()
 	{
 		var code =
-@"using MockTests;
-using Rocks;
-using System;
+			"""
+			using MockTests;
+			using Rocks;
+			using System;
 
-var rock = Rock.Make<ITest>();
+			var rock = Rock.Make<ITest>();
 
-namespace MockTests
-{
-	public interface ITest
-	{
-		void Foo();
-	}
-}";
+			namespace MockTests
+			{
+				public interface ITest
+				{
+					void Foo();
+				}
+			}
+			""";
 
 		var generatedCode =
-@"using Rocks;
-using Rocks.Exceptions;
-using System;
-using System.Collections.Generic;
-using System.Collections.Immutable;
-
-#nullable enable
-namespace MockTests
-{
-	internal static class MakeExpectationsOfITestExtensions
-	{
-		internal static ITest Instance(this MakeGeneration<ITest> self) =>
-			new RockITest();
-		
-		private sealed class RockITest
-			: ITest
-		{
-			public RockITest() { }
+			"""
+			#nullable enable
 			
-			public void Foo()
+			namespace MockTests
 			{
+				internal static class MakeExpectationsOfITestExtensions
+				{
+					internal static global::MockTests.ITest Instance(this global::Rocks.MakeGeneration<global::MockTests.ITest> self) =>
+						new RockITest();
+					
+					private sealed class RockITest
+						: global::MockTests.ITest
+					{
+						public RockITest() { }
+						
+						public void Foo()
+						{
+						}
+					}
+				}
 			}
-		}
-	}
-}
-";
+			
+			""";
 
 		await TestAssistants.RunAsync<RockMakeGenerator>(code,
 			new[] { (typeof(RockMakeGenerator), "ITest_Rock_Make.g.cs", generatedCode) },
@@ -235,20 +232,22 @@ namespace MockTests
 	public static async Task GenerateWhenTargetTypeIsInvalidAsync()
 	{
 		var code =
-@"using Rocks;
+			"""
+			using Rocks;
 
-namespace MockTests
-{
-	public sealed class InvalidTarget { }
+			namespace MockTests
+			{
+				public sealed class InvalidTarget { }
 
-	public static class Test
-	{
-		public static void Generate()
-		{
-			var rock = Rock.Make<InvalidTarget>();
-		}
-	}
-}";
+				public static class Test
+				{
+					public static void Generate()
+					{
+						var rock = Rock.Make<InvalidTarget>();
+					}
+				}
+			}
+			""";
 
 		var diagnostic = new DiagnosticResult(CannotMockSealedTypeDiagnostic.Id, DiagnosticSeverity.Error)
 			.WithSpan(5, 22, 5, 35);
@@ -261,24 +260,26 @@ namespace MockTests
 	public static async Task GenerateWhenTargetTypeHasDiagnosticsAsync()
 	{
 		var code =
-@"using Rocks;
+			"""
+			using Rocks;
 
-namespace MockTests
-{
-	public interface ITest 
-	{ 
-		// Note the missing semicolon
-		void Foo()
-	}
+			namespace MockTests
+			{
+				public interface ITest 
+				{ 
+					// Note the missing semicolon
+					void Foo()
+				}
 
-	public static class Test
-	{
-		public static void Generate()
-		{
-			var rock = Rock.Make<ITest>();
-		}
-	}
-}";
+				public static class Test
+				{
+					public static void Generate()
+					{
+						var rock = Rock.Make<ITest>();
+					}
+				}
+			}
+			""";
 
 		var diagnostic = new DiagnosticResult("CS1002", DiagnosticSeverity.Error)
 			.WithSpan(8, 13, 8, 13);
@@ -291,51 +292,50 @@ namespace MockTests
 	public static async Task GenerateWhenTargetTypeIsValidButOtherCodeHasDiagnosticsAsync()
 	{
 		var code =
-@"using Rocks;
+			"""
+			using Rocks;
 
-namespace MockTests
-{
-	public interface ITest 
-	{ 
-		void Foo();
-	}
+			namespace MockTests
+			{
+				public interface ITest 
+				{ 
+					void Foo();
+				}
 
-	public static class Test
-	{
-		public static void Generate()
-		{
-			var rock = Rock.Make<ITest>();
-		}
-// Note the missing closing brace
-	}";
+				public static class Test
+				{
+					public static void Generate()
+					{
+						var rock = Rock.Make<ITest>();
+					}
+			// Note the missing closing brace
+				}
+			""";
 
 		var generatedCode =
-@"using Rocks;
-using Rocks.Exceptions;
-using System;
-using System.Collections.Generic;
-using System.Collections.Immutable;
-
-#nullable enable
-namespace MockTests
-{
-	internal static class MakeExpectationsOfITestExtensions
-	{
-		internal static ITest Instance(this MakeGeneration<ITest> self) =>
-			new RockITest();
-		
-		private sealed class RockITest
-			: ITest
-		{
-			public RockITest() { }
+			"""
+			#nullable enable
 			
-			public void Foo()
+			namespace MockTests
 			{
+				internal static class MakeExpectationsOfITestExtensions
+				{
+					internal static global::MockTests.ITest Instance(this global::Rocks.MakeGeneration<global::MockTests.ITest> self) =>
+						new RockITest();
+					
+					private sealed class RockITest
+						: global::MockTests.ITest
+					{
+						public RockITest() { }
+						
+						public void Foo()
+						{
+						}
+					}
+				}
 			}
-		}
-	}
-}
-";
+			
+			""";
 
 		var diagnostic = new DiagnosticResult("CS1513", DiagnosticSeverity.Error)
 			.WithSpan(17, 3, 17, 3);
