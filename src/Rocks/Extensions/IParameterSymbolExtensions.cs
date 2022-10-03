696ad97961e3d710f6c9393c97bab48001ef1ca1
Jason Bock <jason.r.bock@outlook.com>
2022-10-02 23:33:04 -0400

    #192 added extension method to determine if null annotations are needed

---
+++ src/Rocks/Extensions/IParameterSymbolExtensions.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.CodeAnalysis;
+
+namespace Rocks.Extensions;
+
+internal static class IParameterSymbolExtensions
+{
+	internal static bool RequiresForcedNullableAnnotation(this IParameterSymbol self) =>
+		self.HasExplicitDefaultValue && self.ExplicitDefaultValue is null &&
+			!self.Type.IsValueType && self.NullableAnnotation != NullableAnnotation.Annotated;
+}
