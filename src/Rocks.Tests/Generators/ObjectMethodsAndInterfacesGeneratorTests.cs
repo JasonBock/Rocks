9497e5f986e489283b74c146236867a56a3216da
Jason Bock <jason.r.bock@outlook.com>
2022-10-22 22:17:56 -0500

    #206 got tests to fail

--- src/Rocks.Tests/Generators/ObjectMethodsAndInterfacesGeneratorTests.cs
+++ src/Rocks.Tests/Generators/ObjectMethodsAndInterfacesGeneratorTests.cs
@@ -6,6 +6,217 @@ namespace Rocks.Tests.Generators;
 public static class ObjectMethodsAndInterfacesGeneratorTests
 {
 	[Test]
+	public static async Task CreateWhenObjectMethodIsHiddenAsStaticAsync()
+	{
+		var code =
+			"""
+			using Rocks;
+
+			#nullable enable
+
+			namespace MockTests
+			{
+				public class StaticToString
+				{
+					protected static new string ToString() => "c";   
+				}
+
+				public static class Test
+				{
+					public static void Generate()
+					{
+						var rock = Rock.Create<StaticToString>();
+					}
+				}
+			}
+			""";
+
+		var generatedCode =
+			"""
+			using Rocks.Extensions;
+			using System.Collections.Generic;
+			using System.Collections.Immutable;
+			#nullable enable
+			
+			namespace MockTests
+			{
+				internal static class CreateExpectationsOfStaticToStringExtensions
+				{
+					internal static global::Rocks.Expectations.MethodExpectations<global::MockTests.StaticToString> Methods(this global::Rocks.Expectations.Expectations<global::MockTests.StaticToString> @self) =>
+						new(@self);
+					
+					internal static global::MockTests.StaticToString Instance(this global::Rocks.Expectations.Expectations<global::MockTests.StaticToString> @self)
+					{
+						if (!@self.WasInstanceInvoked)
+						{
+							@self.WasInstanceInvoked = true;
+							return new RockStaticToString(@self);
+						}
+						else
+						{
+							throw new global::Rocks.Exceptions.NewMockInstanceException("Can only create a new mock once.");
+						}
+					}
+					
+					private sealed class RockStaticToString
+						: global::MockTests.StaticToString
+					{
+						private readonly global::System.Collections.Generic.Dictionary<int, global::System.Collections.Generic.List<global::Rocks.HandlerInformation>> handlers;
+						
+						public RockStaticToString(global::Rocks.Expectations.Expectations<global::MockTests.StaticToString> @expectations) =>
+							this.handlers = @expectations.Handlers;
+						
+						[global::Rocks.MemberIdentifier(0, "bool Equals(object? @obj)")]
+						public override bool Equals(object? @obj)
+						{
+							if (this.handlers.TryGetValue(0, out var @methodHandlers))
+							{
+								foreach (var @methodHandler in @methodHandlers)
+								{
+									if (global::System.Runtime.CompilerServices.Unsafe.As<global::Rocks.Argument<object?>>(@methodHandler.Expectations[0]).IsValid(@obj))
+									{
+										var @result = @methodHandler.Method is not null ?
+											global::System.Runtime.CompilerServices.Unsafe.As<global::System.Func<object?, bool>>(@methodHandler.Method)(@obj) :
+											global::System.Runtime.CompilerServices.Unsafe.As<global::Rocks.HandlerInformation<bool>>(@methodHandler).ReturnValue;
+										@methodHandler.IncrementCallCount();
+										return @result!;
+									}
+								}
+								
+								throw new global::Rocks.Exceptions.ExpectationException("No handlers match for bool Equals(object? @obj)");
+							}
+							else
+							{
+								return base.Equals(@obj);
+							}
+						}
+						
+						[global::Rocks.MemberIdentifier(1, "int GetHashCode()")]
+						public override int GetHashCode()
+						{
+							if (this.handlers.TryGetValue(1, out var @methodHandlers))
+							{
+								var @methodHandler = @methodHandlers[0];
+								var @result = @methodHandler.Method is not null ?
+									global::System.Runtime.CompilerServices.Unsafe.As<global::System.Func<int>>(@methodHandler.Method)() :
+									global::System.Runtime.CompilerServices.Unsafe.As<global::Rocks.HandlerInformation<int>>(@methodHandler).ReturnValue;
+								@methodHandler.IncrementCallCount();
+								return @result!;
+							}
+							else
+							{
+								return base.GetHashCode();
+							}
+						}
+						
+						[global::Rocks.MemberIdentifier(2, "string? ToString()")]
+						public override string? ToString()
+						{
+							if (this.handlers.TryGetValue(2, out var @methodHandlers))
+							{
+								var @methodHandler = @methodHandlers[0];
+								var @result = @methodHandler.Method is not null ?
+									global::System.Runtime.CompilerServices.Unsafe.As<global::System.Func<string?>>(@methodHandler.Method)() :
+									global::System.Runtime.CompilerServices.Unsafe.As<global::Rocks.HandlerInformation<string?>>(@methodHandler).ReturnValue;
+								@methodHandler.IncrementCallCount();
+								return @result!;
+							}
+							else
+							{
+								return base.ToString();
+							}
+						}
+						
+					}
+				}
+				
+				internal static class MethodExpectationsOfStaticToStringExtensions
+				{
+					internal static global::Rocks.MethodAdornments<global::MockTests.StaticToString, global::System.Func<object?, bool>, bool> Equals(this global::Rocks.Expectations.MethodExpectations<global::MockTests.StaticToString> @self, global::Rocks.Argument<object?> @obj)
+					{
+						global::System.ArgumentNullException.ThrowIfNull(@obj);
+						return new global::Rocks.MethodAdornments<global::MockTests.StaticToString, global::System.Func<object?, bool>, bool>(@self.Add<bool>(0, new global::System.Collections.Generic.List<global::Rocks.Argument>(1) { @obj }));
+					}
+					internal static global::Rocks.MethodAdornments<global::MockTests.StaticToString, global::System.Func<int>, int> GetHashCode(this global::Rocks.Expectations.MethodExpectations<global::MockTests.StaticToString> @self) =>
+						new global::Rocks.MethodAdornments<global::MockTests.StaticToString, global::System.Func<int>, int>(@self.Add<int>(1, new global::System.Collections.Generic.List<global::Rocks.Argument>()));
+					internal static global::Rocks.MethodAdornments<global::MockTests.StaticToString, global::System.Func<string?>, string?> ToString(this global::Rocks.Expectations.MethodExpectations<global::MockTests.StaticToString> @self) =>
+						new global::Rocks.MethodAdornments<global::MockTests.StaticToString, global::System.Func<string?>, string?>(@self.Add<string?>(2, new global::System.Collections.Generic.List<global::Rocks.Argument>()));
+				}
+			}
+			
+			""";
+
+		await TestAssistants.RunAsync<RockCreateGenerator>(code,
+			new[] { (typeof(RockCreateGenerator), "StaticToString_Rock_Create.g.cs", generatedCode) },
+			Enumerable.Empty<DiagnosticResult>()).ConfigureAwait(false);
+	}
+
+	[Test]
+	public static async Task MakeWhenObjectMethodIsHiddenAsStaticAsync()
+	{
+		var code =
+			"""
+			using Rocks;
+
+			#nullable enable
+
+			namespace MockTests
+			{
+				public class StaticToString
+				{
+					protected static new string ToString() => "c";   
+				}
+
+				public static class Test
+				{
+					public static void Generate()
+					{
+						var rock = Rock.Make<StaticToString>();
+					}
+				}
+			}
+			""";
+
+		var generatedCode =
+			"""
+			#nullable enable
+			
+			namespace MockTests
+			{
+				internal static class MakeExpectationsOfStaticToStringExtensions
+				{
+					internal static global::MockTests.StaticToString Instance(this global::Rocks.MakeGeneration<global::MockTests.StaticToString> @self) =>
+						new RockStaticToString();
+					
+					private sealed class RockStaticToString
+						: global::MockTests.StaticToString
+					{
+						public RockStaticToString() { }
+						
+						public override bool Equals(object? @obj)
+						{
+							return default!;
+						}
+						public override int GetHashCode()
+						{
+							return default!;
+						}
+						public override string? ToString()
+						{
+							return default!;
+						}
+					}
+				}
+			}
+						
+			""";
+
+		await TestAssistants.RunAsync<RockMakeGenerator>(code,
+			new[] { (typeof(RockMakeGenerator), "StaticToString_Rock_Make.g.cs", generatedCode) },
+			Enumerable.Empty<DiagnosticResult>()).ConfigureAwait(false);
+	}
+
+	[Test]
 	public static async Task GenerateWithExactMatchesCreateAsync()
 	{
 		var code =
