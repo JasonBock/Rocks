7bc558194df95fd9eb8e41bafbf0f272fb52f263
Jason Bock <jason.r.bock@outlook.com>
2023-11-18 11:06:51 -0600

    #274 added more package references

---
+++ src/Rocks.CodeGenerationTest/Mappings/OpenApiMappings.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.OpenApi.Interfaces;
+using Microsoft.OpenApi.Models;
+using Microsoft.OpenApi.Writers;
+
+namespace Rocks.CodeGenerationTest.Mappings
+{
+	internal static class OpenApiMappings
+	{
+		internal static Dictionary<Type, Dictionary<string, string>> GetMappedTypes() =>
+			new()
+			{
+				{
+					typeof(IEffective<>), new()
+					{
+						{ "T", "global::Rocks.CodeGenerationTest.Mappings.Microsoft.OpenApi.MappedOpenApiElement" },
+					}
+				},
+				{
+					typeof(OpenApiExtensibleDictionary<>), new()
+					{
+						{ "T", "global::Rocks.CodeGenerationTest.Mappings.Microsoft.OpenApi.MappedOpenApiSerializable" },
+					}
+				},
+			};
+	}
+
+	namespace Microsoft.OpenApi
+	{
+		public sealed class MappedOpenApiElement
+			: IOpenApiElement
+		{ }
+
+		public sealed class MappedOpenApiSerializable
+			: IOpenApiSerializable
+		{
+			public void SerializeAsV2(IOpenApiWriter writer) => throw new NotImplementedException();
+			public void SerializeAsV3(IOpenApiWriter writer) => throw new NotImplementedException();
+		}
+	}
+}
---
+++ src/Rocks.CodeGenerationTest/Mappings/QuartzMappings.cs
@@ -0,0 +1,81 @@
+﻿using Quartz;
+using Quartz.Impl.Matchers;
+using Quartz.Util;
+
+namespace Rocks.CodeGenerationTest.Mappings
+{
+	internal static class QuartzMappings
+	{
+		internal static Dictionary<Type, Dictionary<string, string>> GetMappedTypes() =>
+			new()
+			{
+				{
+					typeof(AndMatcher<>), new()
+					{
+						{ "TKey", "global::Rocks.CodeGenerationTest.Mappings.Quartz.MappedKey" },
+					}
+				},
+				{
+					typeof(EverythingMatcher<>), new()
+					{
+						{ "TKey", "global::Rocks.CodeGenerationTest.Mappings.Quartz.MappedKey" },
+					}
+				},
+				{
+					typeof(GroupMatcher<>), new()
+					{
+						{ "TKey", "global::Rocks.CodeGenerationTest.Mappings.Quartz.MappedKey" },
+					}
+				},
+				{
+					typeof(IMatcher<>), new()
+					{
+						{ "T", "global::Rocks.CodeGenerationTest.Mappings.Quartz.MappedKey" },
+					}
+				},
+				{
+					typeof(KeyMatcher<>), new()
+					{
+						{ "TKey", "global::Rocks.CodeGenerationTest.Mappings.Quartz.MappedKey" },
+					}
+				},
+				{
+					typeof(NameMatcher<>), new()
+					{
+						{ "TKey", "global::Rocks.CodeGenerationTest.Mappings.Quartz.MappedKey" },
+					}
+				},
+				{
+					typeof(NotMatcher<>), new()
+					{
+						{ "TKey", "global::Rocks.CodeGenerationTest.Mappings.Quartz.MappedKey" },
+					}
+				},
+				{
+					typeof(OrMatcher<>), new()
+					{
+						{ "TKey", "global::Rocks.CodeGenerationTest.Mappings.Quartz.MappedKey" },
+					}
+				},
+				{
+					typeof(ScheduleBuilder<>), new()
+					{
+						{ "T", "global::Quartz.ITrigger" },
+					}
+				},
+				{
+					typeof(StringMatcher<>), new()
+					{
+						{ "TKey", "global::Rocks.CodeGenerationTest.Mappings.Quartz.MappedKey" },
+					}
+				},
+			};
+	}
+
+	namespace Quartz
+	{
+		public sealed class MappedKey
+			: Key<MappedKey>
+		{ }
+	}
+}
