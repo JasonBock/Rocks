0b9275fbe98afe6ec701a912b8e6f56c9d1c53ca
Jason Bock <jason.r.bock@outlook.com>
2023-11-25 08:46:40 -0600

    #281 added an extension method to get unique aliases from a compilation

---
+++ src/Rocks/Extensions/CompilationExtensions.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.CodeAnalysis;
+using System.Collections.Immutable;
+
+namespace Rocks.Extensions;
+
+internal static class CompilationExtensions
+{
+   internal static ImmutableArray<string> GetAliases(this Compilation self) => 
+		self.References.Where(_ => _.Properties.Aliases.Length > 0)
+		   .Select(_ => _.Properties.Aliases[0])
+		   .Distinct()
+		   .OrderBy(_ => _).ToImmutableArray();
+}
