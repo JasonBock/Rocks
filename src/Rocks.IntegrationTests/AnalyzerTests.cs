37c6a397da334c67a85e40f10336315d56f5ea82
Jason Bock <jason.r.bock@outlook.com>
2024-01-25 10:21:08 -0600

    Added a test to check for diagnostic creation from the analyzer.

---
+++ src/Rocks.IntegrationTests/AnalyzerTests.cs
@@ -0,0 +1,19 @@
+﻿namespace Rocks.IntegrationTests;
+
+// This is an odd one, but all I'm doing
+// is making sure that the analyzer is 
+// working correctly. So, I create a type
+// that I know the analyzer should create a diagnostic for,
+// and then put a #pragma around it.
+// Now, there is an informational diagnostic
+// that will state if a #pragma is not needed (IDE0079),
+// but...I can't make that a "warning" (and then an error),
+// which would be nice because then it would tell me
+// if the analyzer wasn't firing. At least
+// I can't figure out how to do that. But this
+// is sufficient for now.
+
+#pragma warning disable ROCK1 // Cannot Mock Sealed Types
+[RockCreate<AnalyzerTests>]
+#pragma warning restore ROCK1 // Cannot Mock Sealed Types
+public sealed class AnalyzerTests { }
