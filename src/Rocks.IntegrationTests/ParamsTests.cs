97e865809b46acc604e68d7b151c30bb8f160a43
Jason Bock <jason.r.bock@outlook.com>
2024-07-29 09:48:30 -0500

    #170 added params integration testing for ReadOnlySpan<>

--- src/Rocks.IntegrationTests/ParamsTests.cs
+++ src/Rocks.IntegrationTests/ParamsTests.cs
@@ -9,10 +9,31 @@ public interface IHaveParams
 {
 	void Foo(int a, params string[] b);
 	int this[int a, params string[] b] { get; }
+	void ParamsFoo(int a, params ReadOnlySpan<string> b);
 }
 
 public static class ParamsTests
 {
+	[Test]
+	public static void CreateMembersWithReadOnlySpanParamsArgumentsSpecified()
+	{
+		var expectations = new IHaveParamsCreateExpectations();
+		expectations.Methods.ParamsFoo(1, 
+			new(_ => _.Length == 2 && _[0] == "b" && _[1] == "c"));
+
+		var mock = expectations.Instance();
+		mock.ParamsFoo(1, "b", "c");
+
+		expectations.Verify();
+	}
+
+	[Test]
+	public static void MakeMembersWithReadOnlySpanParamsArgumentsSpecified()
+	{
+		var mock = new IHaveParamsMakeExpectations().Instance();
+		mock.ParamsFoo(1, "b", "c");
+	}
+
 	[Test]
 	public static void CreateMembersWithParamsArgumentsSpecified()
 	{
