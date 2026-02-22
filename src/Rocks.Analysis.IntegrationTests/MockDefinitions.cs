44a52d0f274a7728b98d0982aac9eff583f1bf22
Jason Bock <jason.r.bock@outlook.com>
2026-02-22 10:09:08 -0600

    #396 CA2025 suppression is complete

--- src/Rocks.Analysis.IntegrationTests/MockDefinitions.cs
+++ src/Rocks.Analysis.IntegrationTests/MockDefinitions.cs
@@ -120,3 +120,4 @@
 [assembly: Rocks.Rock(typeof(Rocks.Analysis.IntegrationTests.ShimTestTypes.IAuditTrail<>), Rocks.BuildType.Create | Rocks.BuildType.Make)]
 [assembly: Rocks.Rock(typeof(Rocks.Analysis.IntegrationTests.RockContextTestTypes.IContextural), Rocks.BuildType.Create | Rocks.BuildType.Make)]
 [assembly: Rocks.Rock(typeof(Rocks.Analysis.IntegrationTests.SuppressCA2012TestTypes.IReturnValueTask), Rocks.BuildType.Create | Rocks.BuildType.Make)]
+[assembly: Rocks.Rock(typeof(Rocks.Analysis.IntegrationTests.SuppressCA2025TestTypes.IReturnDisposableTask), Rocks.BuildType.Create | Rocks.BuildType.Make)]
---
+++ src/Rocks.Analysis.IntegrationTests/SuppressCA2025Tests.cs
@@ -0,0 +1,86 @@
+﻿using NUnit.Framework;
+
+namespace Rocks.Analysis.IntegrationTests.SuppressCA2025TestTypes;
+
+public sealed class Disposable
+	: IDisposable
+{
+	private bool disposedValue;
+
+	private void Dispose(bool disposing)
+	{
+		if (!this.disposedValue)
+		{
+			if (disposing) { }
+
+			this.disposedValue = true;
+		}
+	}
+
+	public void Dispose()
+	{
+		this.Dispose(disposing: true);
+		GC.SuppressFinalize(this);
+	}
+}
+
+public interface IReturnDisposableTask
+{
+	Task<Disposable> DoTaskAsync();
+	ValueTask<Disposable> DoValueTaskAsync();
+}
+
+internal static class SuppressCA2025Tests
+{
+	[Test]
+	public static async Task CreateTaskUsingReturnValueAsync()
+	{
+		using var context = new RockContext();
+		var expectations = context.Create<IReturnDisposableTaskCreateExpectations>();
+		using var disposable = new Disposable();
+		expectations.Setups.DoTaskAsync().ReturnValue(Task.FromResult(disposable));
+
+		var mock = expectations.Instance();
+		var mockDisposable = await mock.DoTaskAsync();
+		Assert.That(mockDisposable, Is.SameAs(disposable));
+	}
+
+	[Test]
+	public static async Task CreateTaskUsingCallbackAsync()
+	{
+		using var context = new RockContext();
+		var expectations = context.Create<IReturnDisposableTaskCreateExpectations>();
+		using var disposable = new Disposable();
+		expectations.Setups.DoTaskAsync().Callback(() => Task.FromResult(disposable));
+
+		var mock = expectations.Instance();
+		var mockDisposable = await mock.DoTaskAsync();
+		Assert.That(mockDisposable, Is.SameAs(disposable));
+	}
+
+	[Test]
+	public static async Task CreateValueTaskUsingReturnValueAsync()
+	{
+		using var context = new RockContext();
+		var expectations = context.Create<IReturnDisposableTaskCreateExpectations>();
+		using var disposable = new Disposable();
+		expectations.Setups.DoValueTaskAsync().ReturnValue(ValueTask.FromResult(disposable));
+
+		var mock = expectations.Instance();
+		var mockDisposable = await mock.DoValueTaskAsync();
+		Assert.That(mockDisposable, Is.SameAs(disposable));
+	}
+
+	[Test]
+	public static async Task CreateValueTaskUsingCallbackAsync()
+	{
+		using var context = new RockContext();
+		var expectations = context.Create<IReturnDisposableTaskCreateExpectations>();
+		using var disposable = new Disposable();
+		expectations.Setups.DoValueTaskAsync().Callback(() => ValueTask.FromResult(disposable));
+
+		var mock = expectations.Instance();
+		var mockDisposable = await mock.DoValueTaskAsync();
+		Assert.That(mockDisposable, Is.SameAs(disposable));
+	}
+}
