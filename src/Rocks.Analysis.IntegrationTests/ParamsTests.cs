0c46d9b39380c1f9c8c26b2fd0917105492e2c18
Jason Bock <jason.r.bock@outlook.com>
2026-08-16 10:12:36 -0500

    #425 small integration code update, tests pass

--- src/Rocks.Analysis.IntegrationTests/ParamsTests.cs
+++ src/Rocks.Analysis.IntegrationTests/ParamsTests.cs
@@ -1,4 +1,4 @@
-﻿//using NUnit.Framework;
+﻿using NUnit.Framework;
 
 namespace Rocks.Analysis.IntegrationTests.ParamsTestTypes;
 
@@ -12,93 +12,93 @@ public interface IHaveParams
 	void ParamsFoo(int a, params ReadOnlySpan<string> b);
 }
 
-//internal static class ParamsTests
-//{
-//	[Test]
-//	public static void CreateMembersWithReadOnlySpanParamsArgumentsSpecified()
-//	{
-//		using var context = new RockContext();
-//		var expectations = context.Create<IHaveParamsCreateExpectations>();
-//		expectations.Setups.ParamsFoo(1,
-//			new RefStructArgument<ReadOnlySpan<string>>(_ => _.Length == 2 && _[0] == "b" && _[1] == "c"));
+internal static class ParamsTests
+{
+	[Test]
+	public static void CreateMembersWithReadOnlySpanParamsArgumentsSpecified()
+	{
+		using var context = new RockContext();
+		var expectations = context.Create<IHaveParamsCreateExpectations>();
+		expectations.Setups.ParamsFoo(1,
+			new RefStructArgument<ReadOnlySpan<string>>(_ => _.Length == 2 && _[0] == "b" && _[1] == "c"));
 
-//		var mock = expectations.Instance();
-//		mock.ParamsFoo(1, "b", "c");
-//	}
+		var mock = expectations.Instance();
+		mock.ParamsFoo(1, "b", "c");
+	}
 
-//	[Test]
-//	public static void CreateMembersWithReadOnlySpanParamsNoVerification()
-//	{
-//		using var context = new RockContext();
-//		var expectations = context.Create<IHaveParamsCreateExpectations>();
-//		expectations.Setups.ParamsFoo(1, new());
+	[Test]
+	public static void CreateMembersWithReadOnlySpanParamsNoVerification()
+	{
+		using var context = new RockContext();
+		var expectations = context.Create<IHaveParamsCreateExpectations>();
+		expectations.Setups.ParamsFoo(1, new());
 
-//		var mock = expectations.Instance();
-//		mock.ParamsFoo(1, "b", "c");
-//	}
+		var mock = expectations.Instance();
+		mock.ParamsFoo(1, "b", "c");
+	}
 
-//	[Test]
-//	public static void MakeMembersWithReadOnlySpanParamsArgumentsSpecified()
-//	{
-//		var mock = new IHaveParamsMakeExpectations().Instance();
-//		mock.ParamsFoo(1, "b", "c");
-//	}
+	[Test]
+	public static void MakeMembersWithReadOnlySpanParamsArgumentsSpecified()
+	{
+		var mock = new IHaveParamsMakeExpectations().Instance();
+		mock.ParamsFoo(1, "b", "c");
+	}
 
-//	[Test]
-//	public static void CreateMembersWithParamsArgumentsSpecified()
-//	{
-//		var returnValue = 3;
-//		using var context = new RockContext();
-//		var expectations = context.Create<IHaveParamsCreateExpectations>();
-//		expectations.Setups.Foo(1, new[] { "b" });
-//		expectations.Setups[1, new[] { "b" }].Gets().ReturnValue(returnValue);
+	[Test]
+	public static void CreateMembersWithParamsArgumentsSpecified()
+	{
+		var returnValue = 3;
+		using var context = new RockContext();
+		var expectations = context.Create<IHaveParamsCreateExpectations>();
+		expectations.Setups.Foo(1, ["b"]);
+		expectations.Setups[1, new[] { "b" }].Gets().ReturnValue(returnValue);
 
-//		var mock = expectations.Instance();
-//		mock.Foo(1, "b");
-//		var value = mock[1, "b"];
+		var mock = expectations.Instance();
+		mock.Foo(1, "b");
+		var value = mock[1, "b"];
 
-//		Assert.That(value, Is.EqualTo(returnValue));
-//	}
+		Assert.That(value, Is.EqualTo(returnValue));
+	}
 
-//	[Test]
-//	public static void MakeMembersWithParamsArgumentsSpecified()
-//	{
-//		var mock = new IHaveParamsMakeExpectations().Instance();
-//		var value = mock[1, "b"];
+	[Test]
+	public static void MakeMembersWithParamsArgumentsSpecified()
+	{
+		var mock = new IHaveParamsMakeExpectations().Instance();
+		var value = mock[1, "b"];
 
-//		using (Assert.EnterMultipleScope())
-//		{
-//			Assert.That(value, Is.Default);
-//			Assert.That(() => mock.Foo(1, "b"), Throws.Nothing);
-//		}
-//	}
+		using (Assert.EnterMultipleScope())
+		{
+			Assert.That(value, Is.Default);
+			Assert.That(() => mock.Foo(1, "b"), Throws.Nothing);
+		}
+	}
 
-//	[Test]
-//	public static void CreateMembersWithParamsArgumentsNotSpecified()
-//	{
-//		var returnValue = 3;
-//		using var context = new RockContext();
-//		var expectations = context.Create<IHaveParamsCreateExpectations>();
-//		expectations.Setups.Foo(1, Array.Empty<string>());
-//		expectations.Setups[1, Array.Empty<string>()].Gets().ReturnValue(returnValue);
+	[Test]
+	public static void CreateMembersWithParamsArgumentsNotSpecified()
+	{
+		var returnValue = 3;
+		using var context = new RockContext();
+		var expectations = context.Create<IHaveParamsCreateExpectations>();
+		expectations.Setups.Foo(1, []);
+		expectations.Setups[1, Array.Empty<string>()].Gets().ReturnValue(returnValue);
 
-//		var mock = expectations.Instance();
-//		mock.Foo(1);
-//		var value = mock[1];
+		var mock = expectations.Instance();
+		mock.Foo(1);
+		var value = mock[1];
 
-//		Assert.That(value, Is.EqualTo(returnValue));
-//	}
+		Assert.That(value, Is.EqualTo(returnValue));
+	}
 
-//	[Test]
-//	public static void MakeMembersWithParamsArgumentsNotSpecified()
-//	{
-//		var mock = new IHaveParamsMakeExpectations().Instance();
-//		var value = mock[1];
+	[Test]
+	public static void MakeMembersWithParamsArgumentsNotSpecified()
+	{
+		var mock = new IHaveParamsMakeExpectations().Instance();
+		var value = mock[1];
 
-//		using (Assert.EnterMultipleScope())
-//		{
-//			Assert.That(value, Is.Default);
-//			Assert.That(() => mock.Foo(1), Throws.Nothing);
-//		}
-//	}
-//}
+		using (Assert.EnterMultipleScope())
+		{
+			Assert.That(value, Is.Default);
+			Assert.That(() => mock.Foo(1), Throws.Nothing);
+		}
+	}
+}
