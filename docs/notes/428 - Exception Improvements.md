d0ef5ad345a9329d412251abcdaf73fa9f737d40
Jason Bock <jason.r.bock@outlook.com>
2026-09-11 13:10:35 -0500

    Minor fix-ups for 10.3.1

---
+++ docs/notes/428 - Exception Improvements.md
@@ -0,0 +1,5 @@
+TODO:
+* Maybe I should add type parameter values as well?
+* Fix all unit tests
+* Ensure all tests pass
+* Add documentation around `ExceptionFailures`, reference `ExpectationExceptionTests.SuppressExceptions()` test as an example
--- src/Rocks.Analysis/Models/ModelContext.cs
+++ src/Rocks.Analysis/Models/ModelContext.cs
@@ -78,18 +78,18 @@ internal sealed class ModelContext
 				this.TypeParameters = [];
 			}
 
-		this.NullableAnnotation = type.NullableAnnotation;
-		this.IsRecord = type.IsRecord;
-		this.IsReferenceType = type.IsReferenceType;
-		this.IsPointer = type.IsPointer();
-		this.IsRefLikeType = type.IsRefLikeType;
-		this.IsTupleType = type.IsTupleType;
+			this.NullableAnnotation = type.NullableAnnotation;
+			this.IsRecord = type.IsRecord;
+			this.IsReferenceType = type.IsReferenceType;
+			this.IsPointer = type.IsPointer();
+			this.IsRefLikeType = type.IsRefLikeType;
+			this.IsTupleType = type.IsTupleType;
 
-		if (type is IArrayTypeSymbol arraySymbol)
-		{
-			this.ArrayElementType = modelContext.CreateTypeReference(arraySymbol.ElementType);
-			this.ArrayRank = arraySymbol.Rank;
-		}
+			if (type is IArrayTypeSymbol arraySymbol)
+			{
+				this.ArrayElementType = modelContext.CreateTypeReference(arraySymbol.ElementType);
+				this.ArrayRank = arraySymbol.Rank;
+			}
 
 			var typeParameterTarget = this.IsPointer ?
 				type.Kind == SymbolKind.PointerType ?
@@ -105,6 +105,14 @@ internal sealed class ModelContext
 				this.PointedAt = new TypeReferenceModel(pointedAt, modelContext);
 				this.PointerNames = string.Concat(Enumerable.Repeat("Pointer", (int)this.PointedAtCount));
 			}
+
+			// XML doc cref rendering for array-typed parameters. ITypeSymbol.Name is
+			// empty for arrays, which produced empty slots (e.g. (String,,CancellationToken)).
+			// Arrays render as Element[] with rank commas; every other kind keeps Name.
+			this.XmlCommentName =
+				this.ArrayElementType is not null ?
+					$"{this.ArrayElementType.XmlCommentName}[{new string(',', this.ArrayRank - 1)}]" :
+					this.Name;
 		}
 
 		private static string BuildName(ITypeReferenceModel current, TypeArgumentsNamingContext parentNamingContext)
@@ -170,6 +178,8 @@ internal sealed class ModelContext
 			this.Equals(other as TypeReferenceModel);
 
 		public bool AllowsRefLikeType { get; }
+		public ITypeReferenceModel? ArrayElementType { get; }
+		public int ArrayRank { get; }
 		public string AttributesDescription { get; }
 		public EquatableArray<Constraints> Constraints { get; }
 		public string FlattenedName { get; }
@@ -194,15 +204,6 @@ internal sealed class ModelContext
 		public EquatableArray<ITypeReferenceModel> TypeArguments { get; }
 		public EquatableArray<ITypeReferenceModel> TypeParameters { get; }
 		public TypeKind TypeKind { get; }
-		public ITypeReferenceModel? ArrayElementType { get; }
-		public int ArrayRank { get; }
-
-		// XML doc cref rendering for array-typed parameters. ITypeSymbol.Name is
-		// empty for arrays, which produced empty slots (e.g. (String,,CancellationToken)).
-		// Arrays render as Element[] with rank commas; every other kind keeps Name.
-		public string XmlCommentName =>
-			this.ArrayElementType is not null ?
-				$"{this.ArrayElementType.XmlCommentName}[{new string(',', this.ArrayRank - 1)}]" :
-				this.Name;
+		public string XmlCommentName { get; }
 	}
 }
